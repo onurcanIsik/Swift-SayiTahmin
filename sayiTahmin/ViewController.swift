d120fdb88735e72d37c6d1fa95d4d35423a28f11
Onurcan <84295038+onurcanIsik@users.noreply.github.com>
2023-08-01 18:49:57 +0300

    Initial Commit

---
+++ sayiTahmin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sayiTahmin
+//
+//  Created by Onurcan Işık on 1.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
